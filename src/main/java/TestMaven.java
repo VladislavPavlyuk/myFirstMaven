7eee8392dba3418e312fa092aab72af8f0cb64cc
Vlad Pavliuk <vladpavliuk@gmail.com>
2025-12-12 12:15:54 +0100

    Update pom.xml to include profiles for development and production, and adjust compiler settings. Enhance TestMaven class to load properties from config.properties.

--- src/main/java/TestMaven.java
+++ src/main/java/TestMaven.java
@@ -2,7 +2,11 @@ import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Multimap;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
+import java.util.Objects;
+import java.util.Properties;
 
 public class TestMaven {
     public static void main(String[] args) {
@@ -16,6 +20,18 @@ public class TestMaven {
         map.put("key", "firstValue");
         map.put("key", "secondValue");
         System.out.println(map);
+
+        Properties prop = new Properties();
+
+        try (InputStream resourceAsStream = TestMaven.class.getClassLoader()
+                .getResourceAsStream("config.properties")) {
+            prop.load(Objects.requireNonNull(resourceAsStream));
+
+            System.out.println(prop.get("props.local.hello"));
+            System.out.println(prop.get("props.mvn.hello"));
+        } catch (IOException e) {
+            System.err.println("Could not load config.properties");
+        }
     }
 }
 
